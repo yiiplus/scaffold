1405f36a60e3aaccfe941e343023d67fc0187e05
gengxiankun <gengxiankun@126.com>
2019-01-30 11:50:15 +0800

    fix(rules.php): 调试代码格式

--- api/config/rules.php
+++ api/config/rules.php
@@ -9,7 +9,7 @@ return [
             ],
         ],
     ],
-	'components' => [
+    'components' => [
         'urlManager' => [
             'enablePrettyUrl' => true,
             'enableStrictParsing' => true,
